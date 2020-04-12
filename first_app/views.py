ddf0007168cb4a4f32870ff82986f93caba6bc50
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2020-04-13 01:26:18 +0530

    understanding django mtv architecture

--- first_app/views.py
+++ first_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html',{ 'insert_me' : 'rahul' })
+    Webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': Webpages_list}
+    return render(request, 'index.html',context = date_dict )
